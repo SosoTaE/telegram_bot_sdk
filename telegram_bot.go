f1880bc96d552d20c0370d5b1d4c89dcea62119c
SosoT <sosotae1615@gmail.com>
2024-12-24 22:15:31 +0400

    message spliting function

--- telegram_bot.go
+++ telegram_bot.go
@@ -5,13 +5,14 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 )
 
 type Bot struct {
 	Token string
 }
 
-func (bot *Bot) SendMessage(message SendMessageType) (map[string]interface{}, error) {
+func (bot *Bot) sendMessage(message SendMessageType) (map[string]interface{}, error) {
 	requestBody, err := json.Marshal(message)
 	if err != nil {
 		return map[string]interface{}{}, err
@@ -38,6 +39,29 @@ func (bot *Bot) SendMessage(message SendMessageType) (map[string]interface{}, er
 	}
 
 	return object, nil
+}
+
+func (bot *Bot) SendMessage(message SendMessageType) (map[string]interface{}, error) {
+	if len(message.Text) > 4096 {
+		text := message.Text[:4096]
+		lastIndex := strings.LastIndex(text, " ")
+		text = text[:lastIndex]
+
+		newMessage := SendMessageType{
+			ChatID: message.ChatID,
+			Text:   text,
+		}
+
+		result, err := bot.sendMessage(newMessage)
+		if err != nil {
+			return result, err
+		}
+
+		return bot.SendMessage(SendMessageType{ChatID: message.ChatID, Text: message.Text[lastIndex+1:]})
+	}
+
+	return bot.sendMessage(message)
+
 }
 
 func (bot *Bot) SendMediaGroup(message SendMediaGroupType) (map[string]interface{}, error) {
